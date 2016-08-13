d0ec3a5dfc824263c165c60c7bf6823338930fe9
nekopunch <liverchild0815@gmail.com>
2016-08-13 17:24:14 +0900

    機能追加/お問い合わせフォーム/確認画面と送信後表示の設置/実験

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ $(document).on('click','#btnFmCnfm',function(){
 	$("#fmInpt>.form-control").each( function() {
 		var title = $(this).attr("name");
 		var val = $(this).val();
-		val = val.replace(/\n/g, "<br />");
+		// val = val.replace(/\n/g, "<br />");
 		$('#dvFmCnfm').append('<p class="white">'+title+'</p>');
 		$('#dvFmCnfm').append('<p class="white">'+val+'</p>');
 	});
