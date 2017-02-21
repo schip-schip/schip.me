bbd7d4a0b96a051483a41a6f0e02f6976904a9ed
Shohei Kodama MacBookPro <is.wider.than.sky@gmail.com>
2017-02-21 15:20:35 +0900

    improve popup-modal size

--- js/main.js
+++ js/main.js
@@ -234,28 +234,10 @@ $(function() {
 		}
 	});
 
-	function centerModal() {
-		$(this).css('display', 'block');
-		var $dialog = $(this).find(".modal-dialog"),
-			offset = ($(window).height() - $dialog.height()) / 2,
-			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
-
-		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to
-		// the bottom margin of the modal
-		if (offset < bottomMargin) offset = bottomMargin;
-		$dialog.css("margin-top", offset);
-	}
-
-	$('.modal').on('show.bs.modal', centerModal);
-
 	$('.modal-popup .close-link').click(function(event){
 		event.preventDefault();
 		$('.modal').modal('hide');
 	});
 
-	$(window).on("resize", function() {
-		$('.modal:visible').each(centerModal);
-	});
 });
 // ********************* END OF TEMPLATE ORIGINAL *********************
